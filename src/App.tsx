89508081fd15983325226febef942a6141f7b0f9
Shubham Mishra <shubham.mishra@mediassistindia.com>
2022-08-15 02:13:26 +0530

    architecture done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Register from "./Auth/Register/Register"
+import Dashboard from "./Home/Dashboard"
+import Login from './Auth/Login/Login';
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const routes = [
+    {
+      path: "*",
+      element:<Login/> ,
+    },
+    {
+      path: "/Register",
+      element: <Register/>,
+    },
+    {
+      path: "/home",
+      element: <Dashboard/>,
+    },
+  ];
+  
+
+return (<>
+    <Router>
+      <Routes>
+      {routes.map((route, index) => (
+      <>
+      <Route path={route.path}  key="index" element={route.element}/>
+        </>
+      ))}
+      </Routes>
+    </Router>
+ </>
   );
 }
 
